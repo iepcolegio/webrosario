98b5bb8c04bbb26e15253262c177318ecf64d47e
iepcolegio <rosario20125asia@gmail.com>
2024-09-09 14:52:32 -0500

    Add files via upload

---
+++ salers3/sweetalert3.js
@@ -0,0 +1,17 @@
+
+
+
+Swal.fire({
+	title:'HORARIO DE ATENCION A PADRES DE FAMILIA',
+confirmButtonText:'CERRAR',
+with: '100%',
+imageUrl:'titulo2.jpg',
+width : '700 %',
+padding:0,
+backdrop:'true',
+allowOutsideClick:false,
+allowEscapekey:false,
+allowEnterkey:true,
+stopkeydownPropagation:false,
+}
+	);
